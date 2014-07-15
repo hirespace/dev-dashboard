3e5bfb4706dc36baa6304246ad355dc8f88b4ceb
Dean <contact@deanhume.com>
2014-07-15 16:36:25 +0100

    Committed initial branch

---
+++ HireSpace.DevDashboard/Controllers/StatsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace HireSpace.DevDashboard.Controllers
+{
+    using System.Net;
+
+    using PusherServer;
+
+    public class StatsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HireSpace.DevDashboard/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HireSpace.DevDashboard.MvcApplication" Language="C#" %>
